e50f6a716c8f7aac75aac810ddecbf5f44471e64
Taiga Miwa <taiga.miwa888@gmail.com>
2021-07-22 12:43:10 +0900

    herokuのZeitwerk::NameErrorへの対応

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module FastlyStudy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.autoload = :classic
 
     # Configuration for the application, engines, and railties goes here.
     #
